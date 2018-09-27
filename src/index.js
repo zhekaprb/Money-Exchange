f0f353e968af7addbe28b2e93ed900f39f461913
zhekaprb <zheka_cool@mail.ru>
2018-09-27 22:23:40 +0300

    20 test passed

--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    if (currency < 0) {
+        return {}
+    } else if (currency > 10000) {
+        return {
+            error: "You are rich, my friend! We don't have so much coins for exchange"
+        }
+    }
+
+    let currencyExchang = {};
+
+    function currencyExchangSetting(object, propertyName, coins) {
+        let numberOfCoins = Math.floor(currency / coins);
+
+        if (numberOfCoins) {
+            object[propertyName] = numberOfCoins;
+            currency = currency % coins;
+        };
+    }
+
+    currencyExchangSetting(currencyExchang, "H", 50);
+    currencyExchangSetting(currencyExchang, "Q", 25);
+    currencyExchangSetting(currencyExchang, "D", 10);
+    currencyExchangSetting(currencyExchang, "N", 5);
+    currencyExchangSetting(currencyExchang, "P", 1);
+
+    return currencyExchang;
 }
